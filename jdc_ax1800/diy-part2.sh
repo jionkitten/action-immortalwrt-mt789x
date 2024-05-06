a4b9e536a9d031f868c43988d87037c175a5606e
jionkitten <jionkitten@outlook.com>
2024-05-06 13:04:40 +0800

    update

--- jdc_ax1800/diy-part2.sh
+++ jdc_ax1800/diy-part2.sh
@@ -1,6 +1,6 @@
 PATCH_FILE_1=$GITHUB_WORKSPACE/jdc_ax1800/0001-show-soc-status-on-luci.patch
 
-cp $PATCHES_FILE_1 $OPENWRT_PATH/feeds/luci
+cp $PATCH_FILE_1 $OPENWRT_PATH/feeds/luci
 cd $OPENWRT_PATH/feeds/luci
 git am 0001-show-soc-status-on-luci.patch
 
