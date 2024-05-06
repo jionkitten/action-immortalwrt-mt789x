25ae1e6dd9d626029c81d8ae102d0205592733c3
jionkitten <jionkitten@outlook.com>
2024-05-06 13:01:14 +0800

    add jdc ax1800

---
+++ jdc_ax1800/diy-part2.sh
@@ -0,0 +1,7 @@
+PATCH_FILE_1=$GITHUB_WORKSPACE/jdc_ax1800/0001-show-soc-status-on-luci.patch
+
+cp $PATCHES_FILE_1 $OPENWRT_PATH/feeds/luci
+cd $OPENWRT_PATH/feeds/luci
+git am 0001-show-soc-status-on-luci.patch
+
+cd $OPENWRT_PATH
