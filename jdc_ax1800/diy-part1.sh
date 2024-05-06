dcfcd8b8efee066f9bd46bec09c1da204710fad3
jionkitten <jionkitten@outlook.com>
2024-05-06 13:17:54 +0800

    update

--- jdc_ax1800/diy-part1.sh
+++ jdc_ax1800/diy-part1.sh
@@ -1,3 +1,3 @@
-echo "src-git my https://github.com/jionkitten/op-packages.git" >> feeds.conf.default
+echo "\nsrc-git my https://github.com/jionkitten/op-packages.git" >> feeds.conf.default
 
 cat feeds.conf.default
